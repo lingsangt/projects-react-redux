fed68f70e334b61d2aac782ab0aaff3878e630d0
Ling Sang <lingsang.tse@mail.utoronto.ca>
2020-05-10 02:52:01 -0400

    added styling for search bar

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 const App = () => {
-    return (<div>
+    return (<div className="ui conatiner" style={{marginTop:'10px'}}>
         <SearchBar />
     </div>);
 };
