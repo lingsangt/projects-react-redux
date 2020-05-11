b162eaa9c8a793deb59d09eae156ad9842d0fd37
Ling Sang <lingsang.tse@mail.utoronto.ca>
2020-05-10 22:38:38 -0400

    added notes on key needed for each item

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 const ImageList = (props) => {
     
+    /*
+    This gives a Warning that Each child in a list should have a unique 
+    'key' prop.
+
+    Whenever we are updating a list to be rendered, the newly rendered list
+    must be matched to items currently in the DOM via keys. We use the ID of 
+    the items as the key for the item because the ID often uniquely identifies 
+    the item.
+    */
     const images = props.images.map ( (image) => {
         return <img src = {image.urls.regular} />;
     });
